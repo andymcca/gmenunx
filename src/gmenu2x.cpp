3035f3ebb5f6f4f256d85078e4e6a614b9a51817
pingflood <pingflood@gmail.com>
2020-02-07 11:16:33 -0300

    Reuse the skin variable

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -859,7 +859,7 @@ void GMenu2X::writeSkinConfig() {
 	ledOff();
 }
 
-void GMenu2X::setSkin(const string &skin, bool clearSC) {
+void GMenu2X::setSkin(string skin, bool clearSC) {
 	confStr["skin"] = skin;
 
 	// Clear previous skin settings
@@ -894,7 +894,6 @@ void GMenu2X::setSkin(const string &skin, bool clearSC) {
 	skinConfColors[COLOR_FONT_ALT_OUTLINE] = (RGBAColor){253,1,252,0};
 
 	// load skin settings
-	string skinconfname = "skins/" + skin + "/skin.conf";
 	if (file_exists(skinconfname)) {
 		ifstream skinconf(skinconfname.c_str(), ios_base::in);
 		if (skinconf.is_open()) {
@@ -923,6 +922,9 @@ void GMenu2X::setSkin(const string &skin, bool clearSC) {
 					} else {
 						skinConfInt[name] = atoi(value.c_str());
 					}
+	skin = "skins/" + skin + "/skin.conf";
+
+	ifstream skinconf(skin.c_str(), ios_base::in);
 				}
 			}
 			skinconf.close();
